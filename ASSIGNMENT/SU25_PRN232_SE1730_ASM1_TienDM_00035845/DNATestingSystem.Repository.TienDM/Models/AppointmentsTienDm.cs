634123839dca3a121f7e9ad01d8b48a3c727e462
CodeCuaTienNe <tiendmse182896@fpt.edu.vn>
2025-05-28 17:02:38 +0700

    relocate gitignore, minimize JSON format of Appoiment

--- ASSIGNMENT/SU25_PRN232_SE1730_ASM1_TienDM_00035845/DNATestingSystem.Repository.TienDM/Models/AppointmentsTienDm.cs
+++ ASSIGNMENT/SU25_PRN232_SE1730_ASM1_TienDM_00035845/DNATestingSystem.Repository.TienDM/Models/AppointmentsTienDm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace DNATestingSystem.Repository.TienDM.Models;
 
@@ -33,11 +34,21 @@ public partial class AppointmentsTienDm
 
     public bool? IsPaid { get; set; }
 
+    // Add display properties for joined data
+    public string? ServiceName { get; set; }
+    public string? StatusName { get; set; }
+    public string? UserName { get; set; }
+
+    // Ignore navigation properties to prevent circular reference
+    [JsonIgnore]
     public virtual AppointmentStatusesTienDm AppointmentStatusesTienDm { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual ICollection<SampleThinhLc> SampleThinhLcs { get; set; } = new List<SampleThinhLc>();
 
+    [JsonIgnore]
     public virtual ServicesNhanVt ServicesNhanVt { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual SystemUserAccount UserAccount { get; set; } = null!;
 }
