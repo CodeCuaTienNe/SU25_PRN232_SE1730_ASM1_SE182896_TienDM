595039d781e567f6a03db68afd9256980efbf4bd
CodeCuaTienNe <tiendmse182896@fpt.edu.vn>
2025-06-12 11:43:07 +0700

    Init AS02

---
+++ ASSIGNMENT/SU25_PRN232_SE1730_ASM2_00035845_TienDM/DNATestingSystem.GraphQLAPIServices.TienDM/GraphQLs/Query.cs
@@ -0,0 +1,29 @@
+﻿using DNATestingSystem.Repository.TienDM.Models;
+using DNATestingSystem.Services.TienDM;
+
+namespace DNATestingSystem.GraphQLAPIServices.TienDM.GraphQLs
+{
+    public class Query
+    {
+        private readonly IServiceProviders _serviceProviders;
+
+        public Query(IServiceProviders serviceProviders)
+        {
+            _serviceProviders = serviceProviders;
+        }
+
+        public async Task<List<AppointmentsTienDm>> GetAllAppointments()
+        {
+            try
+            {
+                var result = await _serviceProviders.AppointmentsTienDmService.GetAllAsync();
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception if needed
+                return new List<AppointmentsTienDm>();
+            }
+        }
+    }
+}
